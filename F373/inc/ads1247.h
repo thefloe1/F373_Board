f53021dedc9e48640d8524bb22fa3693f9c78117
thefloe1 <thefloe@pseudoterminal.org>
2015-08-03 10:41:11 +0200

    moved adc functions ADS1247.c/h

---
+++ F373/inc/ads1247.h
@@ -0,0 +1,22 @@
+/*
+ * ads1247.h
+ *
+ *  Created on: 03.08.2015
+ *      Author: Tobias
+ */
+
+#ifndef ADS1247_H_
+#define ADS1247_H_
+
+#include "stm32f3xx_hal.h"
+#include "spi.h"
+
+
+uint8_t ADS1247_xfer(uint8_t byte);
+void ADS1247_Reset();
+void ADS1247_SELFOCAL();
+uint8_t ADS1247_ReadReg(uint8_t reg);
+void ADS1247_WriteReg(uint8_t reg, uint8_t val);
+uint32_t ADS1247_Value();
+
+#endif /* ADS1247_H_ */
--- F373/src/main.c
+++ F373/src/main.c
@@ -38,6 +38,7 @@
 #include "usb_device.h"
 #include "usbd_core.h"
 #include "gpio.h"
+#include "ads1247.h"
 
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
@@ -58,51 +59,7 @@ void SystemClock_Config(void);
 
 /* USER CODE BEGIN 0 */
 
-uint8_t ADS1247_xfer(uint8_t byte)
-{
-	uint8_t ret;
-	HAL_SPI_TransmitReceive(&hspi1, &byte, &ret, 1, 1000);
-	return ret;
-}
 
-void ADS1247_Reset()
-{
-	ADS1247_xfer(0x06);
-	HAL_Delay(100);
-}
-
-void ADS1247_SELFOCAL()
-{
-	ADS1247_xfer(0x62);
-	HAL_Delay(4000);
-}
-
-uint8_t ADS1247_ReadReg(uint8_t reg)
-{
-  ADS1247_xfer(0x20 | (reg&0x0F));		// read id reg
-  ADS1247_xfer(0x00);				// one reg
-  return ADS1247_xfer(0x00);		// get data
-}
-
-void ADS1247_WriteReg(uint8_t reg, uint8_t val)
-{
-	  ADS1247_xfer(0x40 | (reg&0x0F));	// write reg
-	  ADS1247_xfer(0x00);				// one reg
-	  ADS1247_xfer(val);				// write data
-}
-
-uint32_t ADS1247_Value()
-{
-	uint32_t res = 0;
-
-	ADS1247_xfer(0x12);
-	res = ADS1247_xfer(0xFF);
-	res <<= 8;
-	res |= ADS1247_xfer(0xFF);
-	res <<= 8;
-	res |= ADS1247_xfer(0xFF);
-	return res;
-}
 /* USER CODE END 0 */
 USBD_HandleTypeDef hUsbDeviceFS;
 
